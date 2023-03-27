eb77cdb10afb50e0ce3e365802e7041032440ac8
charlesericbry <charles-eric.bry@umontreal.ca>
2023-03-27 19:32:00 -0400

    Transfert des commentaires et codes de partie 1

--- src/main/java/server/EventHandler.java
+++ src/main/java/server/EventHandler.java
@@ -1,5 +1,7 @@
 package server;
-
+/**
+ * Interface qui relie les différentes méthodes de gestion de commandes
+ */
 @FunctionalInterface
 public interface EventHandler {
     void handle(String cmd, String arg);
--- src/main/java/server/models/Course.java
+++ src/main/java/server/models/Course.java
@@ -1,7 +1,9 @@
 package server.models;
 
 import java.io.Serializable;
-
+/**
+ * Classe contenant les informations d'un cours.
+ */
 public class Course implements Serializable {
 
 
@@ -9,36 +11,70 @@ public class Course implements Serializable {
     private String code;
     private String session;
 
+    /**
+     * Construit un cour.
+     * @param name nom du cour en question
+     * @param code code du cour
+     * @param session session auquel le cour a lieu
+     */
     public Course(String name, String code, String session) {
         this.name = name;
         this.code = code;
         this.session = session;
     }
 
+    /**
+     *
+     * @return retourne le nom
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Permet de changer le nom du cours
+     * @param name nom du cours
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     *
+     * @return retourne le code du cours
+     */
     public String getCode() {
         return code;
     }
 
+    /**
+     * Permet de changer le code du cours
+     * @param code code du cours
+     */
     public void setCode(String code) {
         this.code = code;
     }
 
+    /**
+     *
+     * @return retourne la session du cours
+     */
     public String getSession() {
         return session;
     }
 
+    /**
+     *
+     * @param session Change la session du cours
+     */
     public void setSession(String session) {
         this.session = session;
     }
 
+    /**
+     *
+     * @return retourne les informations du cours dans une seule phrase.
+     */
     @Override
     public String toString() {
         return "Course{" +
