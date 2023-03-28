f4a1742abf744ed41df34849d735f20972021673
charlesericbry <charles-eric.bry@umontreal.ca>
2023-03-27 21:25:00 -0400

    Première connexion client serveur

--- src/main/java/client/Main.java
+++ src/main/java/client/Main.java
@@ -1,31 +1,53 @@
 package client;
 
 import java.net.Socket;
+import java.util.Scanner;
+
 
 public class Main {
 
-    public static void main(String[] args){
-        try{
-            Socket clientSocket = new Socket("127.0.0.1", 1337);
+    public static void main(String[] args) {
+        try {
+            Socket client = new Socket("127.0.0.1", 1337);
             System.out.println("*** Bienvenue au portail d'inscription de cours de l'UDEM ***");
             System.out.println("Veuillez choisir la session pour laquelle vous voulez consulter la liste de cours:");
             System.out.println("1. Automne");
             System.out.println("2. Hiver");
             System.out.println("3. Été");
-            while (true){
-                
+            System.out.print(">Choix:");
+            while (true) {
+                try {
+                    Scanner scanner = new Scanner(System.in);
+                    
+                    int num = scanner.nextInt();
+                    switch (num) {
 
-                //System.out.println(">Choix:"+choixSession);
+                        case 1:
+                            System.out.println("Automne");
+                            break;
+                        case 2:
+                            System.out.println("Hiver");
+                            break;
+                        case 3:
+                            System.out.println("Été");
+                            break;
+                        default:
+                            System.out.println("Commande inconnue, veuillez recommencez");
+                    }
+
+
+                    //System.out.println(">Choix:"+choixSession);
+                } catch (Exception e) {
+                    System.out.println("Commande inconnue. Veuillez entrer un nombre entre 1 et 3. Merci!");
+                    System.out.print(">Choix:");
+                }
+
+
             }
 
 
-
-
-        }catch(Exception e){
-
+        } catch (Exception e) {
+            System.out.println("Raté plus");
         }
-
-
-
     }
 }
