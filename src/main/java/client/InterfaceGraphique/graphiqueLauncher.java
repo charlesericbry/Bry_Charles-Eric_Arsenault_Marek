4221d6c1e5e76330fa031ae0c1907dd84f805856
charlesericbry <charles-eric.bry@umontreal.ca>
2023-04-14 00:00:55 -0400

    commentaires

--- src/main/java/client/InterfaceGraphique/graphiqueLauncher.java
+++ src/main/java/client/InterfaceGraphique/graphiqueLauncher.java
@@ -4,8 +4,19 @@ package client.InterfaceGraphique;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+/**
+ * Classede lancement de l'interface graphique.
+ */
 public class graphiqueLauncher extends Application {
+    /**
+     * Port de l'ordinateur
+     */
     public final static int PORT = 1337;
+
+    /**
+     * Méthode main qui lance l'interface graphique
+     * @param args
+     */
     public static void main(String[] args) {
         try {
 
