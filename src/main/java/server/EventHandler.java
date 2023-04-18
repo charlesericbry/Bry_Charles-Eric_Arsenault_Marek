b93302f8dfb595fd3469022538082308267ab93c
charlesericbry <charles-eric.bry@umontreal.ca>
2023-04-17 20:41:34 -0400

    jars

--- src/main/java/server/EventHandler.java
+++ src/main/java/server/EventHandler.java
@@ -6,3 +6,5 @@ package server;
 public interface EventHandler {
     void handle(String cmd, String arg);
 }
+
+//javaFX/lib/javafx.base.jar javaFX/lib/javafx.controls.jar javaFX/lib/javafx.fxml.jar javaFX/lib/javafx.graphics.jar javaFX/lib/javafx.media.jar javaFX/lib/javafx.properties javaFX/lib/javafx.swing.jar javaFX/lib/javafx.web.jar javaFX/lib/javafx-swt.jar
